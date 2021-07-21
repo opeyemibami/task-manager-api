74d1c7fa57ccf441ff69f2bddd5285cc6c3ddeed
opeyemibami <bamigbadeopeyemi@gmail.com>
2021-07-21 15:37:53 +0100

    update db config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const dbName = "task-manager-api";
+const dbName = 'task-manager-api'
 const connectionUrl = process.env.MONGO_URI + dbName
 
-mongoose.connect(connectionUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(connectionUrl, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    // console.log(`MongoDB Connected ${conn.connection.host}`)
+  } catch (error) {
+    // console.log(`Error Db: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+connectDB()
